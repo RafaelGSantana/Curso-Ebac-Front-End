b6551be3edd4a880eccef9cf5274df7b6795f7f4
Rafael G. Santana <rgs1990419@gmail.com>
2022-09-28 18:04:41 -0300

    adjustments in css and js gulp tasks + add script of my fontawesome kit

--- gulp/projeto/gulpfile.js
+++ gulp/projeto/gulpfile.js
@@ -6,16 +6,27 @@ import uglify from 'gulp-uglify';
 import image from 'gulp-image';
 
 function tarefasCSS(cb) {
-   return gulp.src('./vendor/**/*.css')
-      .pipe(concat('libs.css'))
+   return gulp.src([
+      './node_modules/bootstrap/dist/css/bootstrap.css',
+      './vendor/fontawesome/fontawesome.css',
+      './vendor/owl/css/owl.css',
+      './src/css/style.css'
+   ])
+      .pipe(concat('styles.css'))
       .pipe(cssmin())
-      .pipe(rename({ suffix: '.min' })) // libs.min.css
+      .pipe(rename({ suffix: '.min' })) // styles.min.css
       .pipe(gulp.dest('./dist/css'));
 }
 
 function tarefasJS() {
-   return gulp.src('./vendor/**/*.js')
-      .pipe(concat('libs.js')) // une os arquivos .js das bibliotecas utilizadas
+   return gulp.src([
+      './node_modules/jquery/dist/jquery.js',
+      './node_modules/bootstrap/dist/js/bootstrap.js',
+      './vendor/owl/js/owl.js',
+      './vendor/jquery-mask/jquery.mask.js',
+      './src/js/custom.js'
+   ])
+      .pipe(concat('scripts.js')) // une todos os arquivos .js da aplicação, sejam das bibliotecas utilizadas como o que eu criei
       .pipe(uglify()) // minifica o arquivo libs.js
       .pipe(rename({ suffix: '.min' })) // libs.min.js
       .pipe(gulp.dest('./dist/js')); // Cria o arquivo lib.min.js no diretório dist/js
