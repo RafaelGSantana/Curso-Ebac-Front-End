6f72fa9ad2a3eab3393ed55a61c24cee76c87899
Rafael G. Santana <rgs1990419@gmail.com>
2022-09-29 15:12:02 -0300

    Change project to use commonjs syntax + create html tasks in gulpfile + html file changed to use files in dist folder as reference

--- gulp/projeto/gulpfile.js
+++ gulp/projeto/gulpfile.js
@@ -1,12 +1,14 @@
-import gulp from 'gulp';
-import concat from 'gulp-concat';
-import cssmin from 'gulp-cssmin';
-import rename from 'gulp-rename';
-import uglify from 'gulp-uglify';
-import image from 'gulp-image';
+const { parallel } = require('gulp');
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const cssmin = require('gulp-cssmin');
+const rename = require('gulp-rename');
+const uglify = require('gulp-uglify');
+const image = require('gulp-image');
+const htmlmin = require('gulp-htmlmin');
 
 function tarefasCSS(cb) {
-   return gulp.src([
+   gulp.src([
       './node_modules/bootstrap/dist/css/bootstrap.css',
       './vendor/fontawesome/fontawesome.css',
       './vendor/owl/css/owl.css',
@@ -16,10 +18,12 @@ function tarefasCSS(cb) {
       .pipe(cssmin())
       .pipe(rename({ suffix: '.min' })) // styles.min.css
       .pipe(gulp.dest('./dist/css'));
+
+      return cb();
 }
 
-function tarefasJS() {
-   return gulp.src([
+function tarefasJS(cb) {
+   gulp.src([
       './node_modules/jquery/dist/jquery.js',
       './node_modules/bootstrap/dist/js/bootstrap.js',
       './vendor/owl/js/owl.js',
@@ -30,10 +34,12 @@ function tarefasJS() {
       .pipe(uglify()) // minifica o arquivo libs.js
       .pipe(rename({ suffix: '.min' })) // libs.min.js
       .pipe(gulp.dest('./dist/js')); // Cria o arquivo lib.min.js no diretório dist/js
+
+   return cb();
 }
 
-function tarefasImagem() {
-   return gulp.src('./src/images/*')
+function tarefasImagem(cb) {
+   gulp.src('./src/images/*')
       .pipe(image({
          pngquant: true,
          optipng: false,
@@ -46,8 +52,16 @@ function tarefasImagem() {
          quiet: true
       }))
       .pipe(gulp.dest('./dist/images'))
+
+   return cb();
 }
 
-export {tarefasCSS as styles}
-export {tarefasJS as scripts}
-export {tarefasImagem as images}
+function tarefasHTML(cb) {
+   gulp.src('./src/**/*.html')
+      .pipe(htmlmin({ collapseWhitespace: true }))
+      .pipe(gulp.dest('./dist'))
+
+   return cb();
+}
+
+exports.default = parallel(tarefasHTML, tarefasCSS, tarefasJS, tarefasImagem)
