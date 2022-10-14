26233f956d3c624c1790b72b517ee1af1bea3233
Rafael G. Santana <rgs1990419@gmail.com>
2022-10-14 18:58:44 -0300

    configuration of webpack

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,8 +1,19 @@
+import './styles.scss';
+import img from './matrix.jpg';
+
 function titleComponent() {
    const elementH1 = document.createElement('h1');
-   elementH1.innerHTML = 'Hello World';
-
+   elementH1.innerHTML = 'Hello World2';
+   elementH1.classList.add('title');
    return elementH1;
 }
 
+function imageComponent() {
+   const elementImg = new Image();
+   elementImg.src = img;
+   return elementImg;
+
+}
+
 document.body.appendChild(titleComponent())
+document.body.appendChild(imageComponent())
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpack = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
    entry: './src/index.js', // primeiro arquvio a ser processado
@@ -6,5 +8,52 @@ module.exports = {
    output: {
       filename: 'main.js', // Arquivo gerado a partir do index.js, podendo ser qualquer nome.js
       path: path.resolve(__dirname, 'dist') // Local onde o arquivo main.js será salvo
-   }
+   },
+   module: {
+      rules: [
+         {
+            test: /\.(sa|c|sc)ss/,
+            use: [
+               MiniCssExtractPlugin.loader,
+               'css-loader',
+               'sass-loader'
+            ]
+         },
+         {
+            test: /\.css$/i,
+            use: [
+               'style-loader',
+               'css-loader'
+            ]
+         },
+         {
+            test: /\.js$/i,
+            exclude: /node_modules/,
+            use: {
+               loader: 'babel-loader',
+               options: {
+                  presets: ['@babel/preset-env']
+               }
+            }
+         },
+         {
+            test: /\.(jpe|jp|pn|sv)g|gif$/i,
+            use: {
+               loader: 'file-loader',
+               options: {
+                  name: '[name].[ext]'
+               }
+            }
+         }
+      ]
+   },
+   plugins: [
+      new HtmlWebpack({
+         filename: 'index.html', // arquivo html empacotado gerado
+         template: './src/index.html', // arquivo que será utilizado para gerar o arquivo index.html empacotado
+      }),
+      new MiniCssExtractPlugin({
+         filename: 'styles.css'
+      })
+   ]
 }
