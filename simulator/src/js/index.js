edb8b417bec5d3efc21bf83c2eb92d499d97d26a
Rafael G. Santana <rgs1990419@gmail.com>
2022-10-18 20:36:37 -0300

    practise with object-fit & object-position

--- simulator/src/js/index.js
+++ simulator/src/js/index.js
@@ -1,4 +1,21 @@
 import '../scss/styles.scss'
+import imageBuilding from '../assets/building.jpg';
+import imageDog from '../assets/cute-dog.jpg';
 
-const elemRoot = document.getElementById('root')
-elemRoot.classList.add('container')
+const elemRoot = document.getElementById('root');
+elemRoot.classList.add('container');
+
+// Define o elemento <img src={imageBuilding} alt="" />
+const elementImageBuilding = new Image();
+elementImageBuilding.src = imageBuilding;
+
+// Coloca o elemento 'elementImageBuilding' dentro da div#building
+document.querySelector("#building").appendChild(elementImageBuilding);
+
+// Define o elemento <img src={imageDog} alt="" />
+const elementImageDog = new Image();
+elementImageDog.src = imageDog;
+
+// Coloca o elemento 'imageDog' dentro da div#building
+document.querySelector("#dog").appendChild(elementImageDog);
+
