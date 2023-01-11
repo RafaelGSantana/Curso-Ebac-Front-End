46283a38cb145c50519d3f12620d6ccece50d631
Rafael G. Santana <rgs1990419@gmail.com>
2023-01-11 19:06:03 -0300

    Aula CodeTogether - mod. ajax

--- ajax/js/form.js
+++ ajax/js/form.js
@@ -3,6 +3,10 @@ const name = document.querySelector('#name');
 const email = document.querySelector('#email');
 const cpf = document.querySelector('#cpf');
 const cep = document.querySelector('#cep');
+const rua = document.querySelector('#rua');
+const bairro = document.querySelector('#bairro');
+const cidade = document.querySelector('#cidade');
+const uf = document.querySelector('#uf');
 const mensagem = document.querySelector('#message');
 const inputsRequired = document.getElementsByClassName('required');
 
@@ -21,6 +25,26 @@ function checkIfEmailIsValid(element) {
       : `Digite um <strong>e-mail</strong> válido`;
 }
 
+function checkIfCepIsValid(element) {
+   if (!element.value.match(/^[0-9]{8}$/)) {
+      return `Digite um cep válido`;
+   } else {
+      return ''
+   }
+
+}
+
+function updateAddress(data) {
+   if (!('erro' in data)) {
+      rua.value = (data.logradouro);
+      bairro.value = (data.bairro);
+      cidade.value = (data.localidade);
+      uf.value = (data.uf);
+      mensagem.innerHTML = '';
+   } else {
+      `Cep não encontrado`
+   }
+}
 form.addEventListener('submit', function (event) {
    event.preventDefault();
 
@@ -37,11 +61,26 @@ form.addEventListener('submit', function (event) {
 
    const isEmailValid = checkIfEmailIsValid(email);
    if (isEmailValid) {
-      msg.push(isEmailValid)
+      msg.push(isEmailValid);
    }
+
+   const isCepValid = checkIfCepIsValid(cep);
+   if (isCepValid) {
+      msg.push(isCepValid);
+   } else {
+      const script = document.createElement('script');
+      script.src = 'https://viacep.com.br/ws/' + cep.value + '/json?callback=updateAddress';
+      document.body.appendChild(script);
+   }
+
+
 
    msg.forEach(item => {
       markUp += `<p>${item}</p>`
    })
    mensagem.innerHTML = markUp;
+
+   // if (msg.length == 0) {
+   //    form.submit();
+   // }
 })
